52a91c550976796ca706e5e17b25a5cf898af10c
anhnv <38343906+thienan0789@users.noreply.github.com>
2019-12-27 20:36:49 +0700

    added Conditional.java
    vi du ve lenh dieu kien trong java

---
+++ SS02-27122019/src/demo2/Conditional.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Conditional {
+
+    public static void main(String[] args) {
+        // Nhap vao 3 so: So luong ban, Moc so sanh, Thuong
+        // Neu So luong ban >= 2 * Moc so sanh thi hien thi "Excellance" va Thuong = Moc so sanh * 2
+        // Neu So luong ban >= 1 * Moc so sanh thi hien thi "Good" va Thuong = Moc so sanh
+        // Neu So luong ban < 1 * Moc so sanh thi hien thi "Bad" va Thuong = 0
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---Kiem tra thuong tet nam nay---");
+        System.out.print("Nhap so tien Moc so sanh: ");
+        int target = scanner.nextInt();
+        System.out.print("Nhap So tien ban duoc: ");
+        int sale = scanner.nextInt();
+        int bonus;
+
+        if (sale >= (2 * target)) {
+            System.out.println("Excellance!!!");
+            bonus = target * 2;
+            System.out.println("Thuong = " + bonus);
+        } else if (sale >= target) {
+            System.out.println("Good!");
+            bonus = target;
+            System.out.println("Thuong = " + bonus);
+        } else {
+            System.out.println("Bad.");
+            bonus = 0;
+            System.out.println("Thuong = " + bonus);
+        }
+        System.out.println("OK. Nguoi tiep theo.");
+    }
+}
--- SS02-27122019/src/demo2/TypesOfVariables.java
+++ SS02-27122019/src/demo2/TypesOfVariables.java
@@ -12,21 +12,22 @@ import java.util.Scanner;
  * @author skyftek
  */
 public class TypesOfVariables {
+
     // 2 cach khai bao enum
     enum Season {
         SPRING(1), SUMMER(2), FALL(3), WINTER(4);
-    
+
         int value;
-        
-        Season (int value){
+
+        Season(int value) {
             this.value = value;
         }
     }
-    
+
     enum Season2 {
         SPRING, SUMMER, FALL, WINTER;
     }
-    
+
     public static void main(String[] args) {
         // Bien nguyen thuy kieu so nguyen
         byte numByte = 1;
@@ -35,61 +36,80 @@ public class TypesOfVariables {
         long longNum = 11111L;
         System.out.println("num = " + longNum);
         // Ket qua: num = 11111
-        
+
+        // Ep kieu ro rang
+        num = (int) longNum;
+        System.out.println(num);
+        // Ket qua: 11111
+
+        num = 5;
+
+        // Ep kieu an du
+        longNum = num;
+        System.out.println(longNum);
+        // Ket qua: 5
+
         // Bien nguyen thuy kieu so thuc
         float numFloat = 0f;
         double numDouble = 0;
-    
+
         // Bien character
         char c = 'A';
-        
+
         // Bien true/false
         boolean type = false;
-        
+
         System.out.println("String \"hello world\"");
         // Ket qua: String "hello world"
-        
-        num = 1000;
-        
-        final int numFinal = 10;
-        
-//        numFinal = 100; [Loi] Vi bien hang khong the khai bao lai
 
+        // Bien hang final - bien chi gan gia tri 1 lan duy nhat khi khoi tao
+        // Bien "num" co the gan lai gia tri nhieu lan
+        num = 1000;
+        // Khai bao bien hang "numFinal"
+        final int numFinal = 10;
+
+//        numFinal = 100; [Loi] Vi bien hang khong the khai bao lai
+        // Goi den bien PI tu class HinhTron
         float s = HinhTron.PI * 4;
         System.out.println(s);
         // Ket qua: 12.56
 
-        // mau enum
+        // Mau enum
         Season season = Season.SUMMER;
         System.out.println("Season = " + season);
         // Ket qua: SUMMER
-        
+
         Season2 season2 = Season2.SUMMER;
-        if(season2.toString().equals("SUMMER")){
+        if (season2.toString().equals("SUMMER")) {
             System.out.println("True");
         } else {
             System.out.println("False");
         }
         // Ket qua: True
-        
+
         // Lay du lieu tu ban phim nhap vao
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập 1 số: ");
-        //  bat gia tri la so nhap vao tu ban phim
-        //  chi nhan gia tri la so, khong nhan cac ky tu khac
+        //  Bat gia tri la so nhap vao tu ban phim
+        //  Chi nhan gia tri la so, khong nhan cac ky tu khac
         int input = scanner.nextInt();
-        //  in ra man hinh gia tri so vua nhap vao
+        //  In ra man hinh gia tri so vua nhap vao
         System.out.println("So da nhap vao: " + input);
-        
-        //  bat gia tri khi su dung Enter
+
+        //  Bat gia tri khi su dung Enter
         scanner.nextLine();
-        //  in ra gia tri trong de cach 1 dong
+        //  In ra gia tri trong de cach 1 dong
         System.out.println("");
-        
+
         System.out.print("Nhap vao ky tu hoac chuoi: ");
-        //  bat gia tri la ky tu hoac chuoi nhap vao
+        //  Bat gia tri la ky tu hoac chuoi nhap vao
         String inputString = scanner.nextLine();
-        //  in ra man hinh ky tu hoac chuoi nhap vao
-        System.out.println("Chuoi da nhap vao: " + inputString);
+
+        if (inputString != null) {
+            System.out.println("Ban da khong nhap gi ca.");
+        } else {
+            //  In ra man hinh ky tu hoac chuoi nhap vao
+            System.out.println("Chuoi da nhap vao: " + inputString);
+        }
     }
 }
